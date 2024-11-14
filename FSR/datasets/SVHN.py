a59acc1826ec4897915bdb3f6d78cbdf124191da
Karen Guo <karenguo98@gmail.com>
2024-11-14 17:59:38 -0500

    add svhn to datasets

---
+++ FSR/datasets/SVHN.py
@@ -0,0 +1,31 @@
+from .base_dataset import BaseDataset
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+import torchvision
+import torch
+
+class SVHN(BaseDataset):
+    def __init__(self, args):
+        self.num_classes = 10
+        self.image_size = (32, 32)
+        self.transform_train = transforms.Compose([
+            transforms.ToTensor(),])
+    
+        self.transform_test = transforms.Compose([
+        transforms.ToTensor(),])
+
+        self.trainset = torchvision.datasets.SVHN(
+            root='./data', split='train', download=True, transform=transform_train)
+        self.trainloader = torch.utils.data.DataLoader(
+            self.trainset, batch_size=args.bs, shuffle=True)
+
+        self.testset = torchvision.datasets.SVHN(
+            root='./data', split='test', download=True, transform=transform_test)
+        self.testloader = torch.utils.data.DataLoader(
+            self.testset, batch_size=args.bs, shuffle=False)
+        
+    def get_dataset(self):
+        return (self.num_classes, self.image_size, 
+                self.transform_train, self.transform_test,
+                self.trainloader, self.testloader, 
+                self.trainset, self.testset)
--- FSR/datasets/__init__.py
+++ FSR/datasets/__init__.py
@@ -1,6 +1,7 @@
 from .CIFAR10 import CIFAR10
 from .CIFAR100 import CIFAR100
 from .MNIST import MNIST
+from .SVHN import SVHN
 
 __all__ = ['available_datasets']
-available_datasets = {'cifar10': CIFAR10 , 'cifar100': CIFAR100, 'mnist': MNIST}
+available_datasets = {'cifar10': CIFAR10 , 'cifar100': CIFAR100, 'mnist': MNIST, 'svhn': SVHN }
